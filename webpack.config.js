87fd599a61ef460e0dbc01d074c00c5bd4974cbe
e-sacredgrounds <kuhasuki@gmail.com>
2017-07-07 01:35:10 -0700

    Remove brunch add webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./web/static/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "priv/static/js"),
+    filename: "app.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader",
+      query: {
+        presets: ["es2015"]
+      }
+    }]
+  }
+};
