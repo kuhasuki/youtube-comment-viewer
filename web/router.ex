90f2ef4f561e6d02994f2f19f5218c118ee7a3a6
e-sacredgrounds <kuhasuki@gmail.com>
2017-07-07 03:54:08 -0700

    add logout functionality

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule YtComments.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug YtComments.Plugs.SetUser
   end
 
   pipeline :api do
@@ -22,6 +23,7 @@ defmodule YtComments.Router do
   scope "/auth", YtComments do
     pipe_through :browser
     
+    get "/signout", AuthController, :delete
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :new
   end
