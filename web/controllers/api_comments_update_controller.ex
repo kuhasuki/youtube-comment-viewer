d361b1b7327b8637cb0ab7b6ab94c0788fed1404
e-sacredgrounds <kuhasuki@gmail.com>
2017-07-08 12:53:11 -0700

    Fix swapped filenames

--- web/controllers/api_comments_update_controller.ex
+++ web/controllers/api_comments_update_controller.ex
@@ -1,18 +1,10 @@
-defmodule YtComments.ApiCommentsController do
+defmodule YtComments.ApiCommentsUpdateController do
   use YtComments.Web, :controller
 
   def index(conn, params) do
-    live_stream_json = HTTPoison.get! "https://www.googleapis.com/youtube/v3/videos?id=#{params["id"]}&key=#{System.get_env("YOUTUBE_API_KEY")}&part=liveStreamingDetails"
-    stream_data = Poison.decode!(live_stream_json.body, keys: :atoms)
+    chat_update_json = HTTPoison.get! "https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=#{params["id"]}&part=id,snippet,authorDetails&key=#{System.get_env("YOUTUBE_API_KEY")}&pageToken=#{params["token"]}"
+    response = Poison.decode!(chat_update_json.body, keys: :atoms)
 
-    # check if response contains activeLiveChatId (some streams disable chat)
-    if Map.has_key?(hd(stream_data.items).liveStreamingDetails, :activeLiveChatId) do
-      chat_id = hd(stream_data.items).liveStreamingDetails.activeLiveChatId
-      initial_chat_json = HTTPoison.get! "https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=#{chat_id}&part=id,snippet,authorDetails&key=#{System.get_env("YOUTUBE_API_KEY")}"
-      response = Poison.decode!(initial_chat_json.body, keys: :atoms)
-    else 
-      response = %{"error" => "This stream has disabled live chat"}
-    end
     render(conn, "comments.json", comments: response)
   end
 end
