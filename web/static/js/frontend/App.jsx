2ca8f2e071ff38d05c9d5acfd18cf8c3d6db9b59
e-sacredgrounds <kuhasuki@gmail.com>
2017-07-09 12:50:51 -0700

    Change initial app to match assignment criteria

--- web/static/js/frontend/App.jsx
+++ web/static/js/frontend/App.jsx
@@ -1,26 +1,38 @@
 import React, { Component } from 'react'
-console.log(React);
 import ReactDOM from 'react-dom'
-import { observer } from 'mobx-react';
+import { observer } from 'mobx-react'
 
-import DevTools from 'mobx-react-devtools';
+import { StreamTile } from './StreamTile'
+import { Comments } from './Comments'
 
 @observer
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.onReset}>
-          Seconds passed: {this.props.appState.timer}
-        </button>
-        <DevTools />
-      </div>
-    );
-  }
 
-  onReset = () => {
-    this.props.appState.resetTimer();
+  render() {
+  	if (this.props.appState.isLoadingStreams) {
+  		return(
+  			<div>Loading</div>
+  		);
+  	} else {
+  		if (this.props.appState.commentView) {
+  			return(
+  				<Comments appState={this.props.appState} />
+  			);
+  		} else {
+		    return (
+	        <div className="mdl-grid">
+	          {this.props.appState.streams.map((streamInstance, index) => {
+							return(
+								<div className="mdl-cell mdl-cell--3-col" key={index} >
+									<StreamTile appState={this.props.appState} title={streamInstance.title} thumbnailUrl={streamInstance.thumbnailUrl} videoId={streamInstance.id} />
+								</div>
+							);
+						})}
+	        </div>
+	    	);	
+	  	}
+  	}
   }
 };
 
-export default App;
+export default App
