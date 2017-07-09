802eede4648b01a6ea846fcfc8e0b46d3c30d708
e-sacredgrounds <kuhasuki@gmail.com>
2017-07-09 12:47:04 -0700

    update test for landing page

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule YtComments.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Youtube Live Stream Comment App"
   end
 end
